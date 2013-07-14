e67ed5d3de8287be152266863f5f784e8fb42ff7
James Fickel <jamesfickel@gmail.com>
2013-07-14 14:00:49 -0700

    Initial rails skeleton with devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gameway::Application.config.secret_key_base = '74ecf2b1a3f40e910ad9df1174288e6794ffe49a77f32512c479b63253288e1b9590c4b983a0954be1f9ae098cbf77948b01410067eb41a01d1b141ae3b1006c'

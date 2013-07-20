2f6b891dbec0fb2131171301f3b763fb9f8fb7c1
James Fickel <jamesfickel@gmail.com>
2013-07-20 16:51:54 -0700

    used strong parameters for devise so signin/login permits fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
 
   config.to_prepare do
     Devise::SessionsController.skip_before_filter :authenticate_user, only: [:new]
     Devise::RegistrationsController.skip_before_filter :authenticate_user, only: [:new]
+  end
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email, :password) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
   end
 end

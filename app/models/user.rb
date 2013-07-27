cb70e25bfa0f7ac527602083d64dc5b1c8d41b4f
James Fickel <jamesfickel@gmail.com>
2013-07-27 13:45:54 -0700

    adding site strings to en.yml and starting omniauth config for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
-         :validatable, :authentication_keys => [:login]
+         :validatable, :authentication_keys => [:login],
+         :omniauthable
+
   attr_accessor :login
   validates :first_name, :last_name, presence: true
 
@@ -15,5 +17,35 @@ class User < ActiveRecord::Base
     else
       where(conditions).first
     end
+  end
+
+  def self.find_for_omniauth(omniauth_response, signed_in_resource=nil)
+    omniauth_info = omniauth_response.info
+
+    user = if (omniauth_info.email.present?)
+             User.find_by_email(omniauth_info.email)
+           elsif (omniauth_info.nickname.present?)
+             User.find_by_twitter_username(omniauth_info.nickname)
+           end
+
+    unless user # Create a user with a stub password.
+      user = User.new(
+        :email => omniauth_info.email,
+        :name => omniauth_info.name,
+        :twitter_username => omniauth_info.nickname
+      )
+    end
+    user
+  end
+
+  def self.new_with_session(params, session)
+    if ((omniauth_data = session["devise.omniauth_data"].info) rescue nil)
+      replaced = {}
+      replaced[:name] = omniauth_data.name if omniauth_data.name.present?
+      replaced[:email] = omniauth_data.email if omniauth_data.email.present?
+      replaced[:twitter_username] = omniauth_data.nickname if omniauth_data.nickname.present?
+      params.merge!(replaced)
+    end
+    super
   end
 end

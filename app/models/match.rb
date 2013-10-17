335e2890e3e87e154e41d80b9e7f83fbceb09159
James Fickel <jamesfickel@gmail.com>
2013-10-17 05:03:52 -0700

    added validations to user_showings so that matches can only have 2 user_showings

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,7 +1,7 @@
 class Match < ActiveRecord::Base
   # remember to delete tournament_members
   belongs_to :tournament
-  has_many :user_showings
+  has_many :user_showings, inverse_of: :match
   has_many :users, through: :user_showings
   has_many :team_showings
   has_many :teams, through: :team_showings
--- app/models/user_showing.rb
+++ app/models/user_showing.rb
@@ -1,4 +1,11 @@
 class UserShowing < ActiveRecord::Base
   belongs_to :user
-  belongs_to :match
+  belongs_to :match, inverse_of: :user_showings
+  validate :maximum_of_two_user_showings_per_match
+
+  def maximum_of_two_user_showings_per_match
+    if match.user_showings.count == 2 || match.user_showings.length > 2
+      errors.add(:user_showing, "can't have more than 2 user showings")
+    end
+  end
 end

83e023ac825b9209580c82aac2768531249c36bf
James Fickel <jamesfickel@gmail.com>
2013-10-17 05:33:01 -0700

    added validations to user_showings so that you cannot have two of the same user in a single match

--- app/models/user_showing.rb
+++ app/models/user_showing.rb
@@ -1,11 +1,21 @@
 class UserShowing < ActiveRecord::Base
   belongs_to :user
   belongs_to :match, inverse_of: :user_showings
-  validate :maximum_of_two_user_showings_per_match
+  validates :user_id, presence: true
+  validate :maximum_of_two_user_showings_per_match, :cannot_have_same_user_in_single_match
 
   def maximum_of_two_user_showings_per_match
     if match.user_showings.count == 2 || match.user_showings.length > 2
-      errors.add(:user_showing, "can't have more than 2 user showings")
+      errors.add(:user_showing, "can't have more than 2 user showings per match")
     end
+  end
+
+  def cannot_have_same_user_in_single_match
+    if match.user_showings.where(user_id: user_id).count > 0 || match.user_showings.select {|us| us.user_id == user_id }.length > 1
+      errors.add(:user_showing, "can't have more than 1 of the same user per match")
+    end
+  end
+
+  def cannot_advance_both_players_from_the_same_previous_match
   end
 end

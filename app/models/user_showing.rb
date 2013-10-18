069ea7c2f1875e4baca28b29b9ffb6a88e2f124a
James Fickel <jamesfickel@gmail.com>
2013-10-17 18:18:11 -0700

    added validation so that you can't advance both players from the same match

--- app/models/user_showing.rb
+++ app/models/user_showing.rb
@@ -2,7 +2,8 @@ class UserShowing < ActiveRecord::Base
   belongs_to :user
   belongs_to :match, inverse_of: :user_showings
   validates :user_id, presence: true
-  validate :maximum_of_two_user_showings_per_match, :cannot_have_two_of_the_same_user_in_single_match
+  validate :maximum_of_two_user_showings_per_match, :cannot_have_two_of_the_same_user_in_single_match,
+  :cannot_advance_both_players_from_the_same_match
 
   def maximum_of_two_user_showings_per_match
     if match.user_showings.count == 2 || match.user_showings.length > 2
@@ -16,6 +17,9 @@ class UserShowing < ActiveRecord::Base
     end
   end
 
-  def cannot_advance_both_players_from_the_same_previous_match
+  def cannot_advance_both_players_from_the_same_match
+    if UserShowing.where(user_id: user_id).last.match.user_showings.select {|last_us| match.user_showings.any? { |us| last_us.user_id == us.user_id }  }.length > 0
+      errors.add(:user_showing, "can't have both players from the same previous match advance")
+    end
   end
 end

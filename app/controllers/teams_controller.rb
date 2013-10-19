15abecabf9dd21a6893ce9b8b34037b334a2bb80
James Fickel <jamesfickel@gmail.com>
2013-10-18 21:13:13 -0700

    team creation now works and added some validations to the process

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,4 +6,25 @@ class TeamsController < ApplicationController
   def new
     @team = Team.new
   end
+
+  def create
+    team = Team.new(name: team_params[:name].split.map(&:capitalize).join(' '))
+    team.team_memberships << TeamMembership.new(user_id: current_user)
+    if team.save
+      redirect_to team_path(team)
+    else
+      flash[:alert] = team.errors.full_messages
+      redirect_to new_team_path
+    end
+  end
+
+  def show
+    @team = Team.find(params[:id])
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name)
+  end
 end

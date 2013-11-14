e7f569df6cc0dd3d6b9137fe97d45ab2935329b4
James Fickel <jamesfickel@gmail.com>
2013-11-13 22:50:27 -0800

    extracted BracketController class out of Tournament

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,14 +1,14 @@
 class SlotsController < ApplicationController
   def create
     tournament = Tournament.includes(:matches).find(params[:id])
-    tournament.advance(params[:position].map{|e| e.to_i })
+    BracketController.new(tournament).advance(params[:position].map{|e| e.to_i })
     tournament.reload
     render json: tournament
   end
 
   def destroy
     tournament = Tournament.includes(:matches).find(params[:id])
-    tournament.delete_slot(params[:destroy].map{|e| e.to_i })
+    BracketController.new(tournament).delete_slot(params[:destroy].map{|e| e.to_i })
     tournament.reload
     render json: tournament
   end

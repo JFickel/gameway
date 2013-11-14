c7b4aaa2cb690e722de457779511654b4f281db9
James Fickel <jamesfickel@gmail.com>
2013-11-13 22:39:57 -0800

    extracting Bracket class out of Tournament -- wip

--- app/models/bracket.rb
+++ app/models/bracket.rb
@@ -1,3 +1,99 @@
 class Bracket
+  attr_accessor :tournament, :tournament_memberships, :output
+  def initialize(tournament)
+    @tournament = tournament
+    @tournament_memberships = tournament.tournament_memberships
+  end
 
+  def rounds
+    Math.log2(tournament_memberships.count).ceil
+  end
+
+  def construct
+    # Builds round arrays. Fills first (filter) and second (filitered) rounds with matches and showings
+    construct_round_arrays
+    fill_round_arrays(filter_slots)
+    return @output
+  end
+
+  private
+
+  def fill_round_arrays(filter_slots)
+    @output[0][0..((filter_slots/2)-1)] = add_filled_matches_to_filter_round(filter_slots)
+
+    if tournament_memberships[filter_slots..-1].length.odd?
+      @output[1][(filter_slots/4)..-1] = build_filtered_round_matches_with_odd_remainder(filter_slots)
+    else
+      @output[1][(filter_slots/4)..-1] = build_filtered_round_matches_with_even_remainder(filter_slots)
+    end
+  end
+
+  def filtered_round_size
+    # Calculates number of contestants in second (filtered) round
+    2**(rounds - 1)
+  end
+
+  def filter_slots
+    # Calculates number of contestants in first (filter) round
+    (tournament_memberships.count - filtered_round_size)*2
+  end
+
+  def add_participant_pair_to_match(match, pair)
+    if tournament.mode == 'individual'
+      match.user_showings.push UserShowing.new(user_id: pair[0].user_id, top: true), UserShowing.new(user_id: pair[1].user_id)
+    elsif tournament.mode == 'team'
+      match.team_showings.push TeamShowing.new(team_id: pair[0].team_id, top: true), TeamShowing.new(team_id: pair[1].team_id)
+    end
+  end
+
+  def add_first_initialized_filtered_round_participant_to_match(match, filter_slots)
+    if tournament.mode == 'individual'
+      match.user_showings.push UserShowing.new(user_id: tournament_memberships[filter_slots..-1].first.user_id)
+    elsif tournament.mode == 'team'
+      match.team_showings.push TeamShowing.new(team_id: tournament_memberships[filter_slots..-1].first.team_id)
+    end
+  end
+
+  def add_filled_matches_to_filter_round(filter_slots)
+    tournament_memberships.first(filter_slots).each_slice(2).with_object([]) do |pair,obj|
+      match = Match.new
+      tournament.matches << match
+      add_participant_pair_to_match(match, pair)
+      obj << match
+    end
+  end
+
+  def build_filtered_round_matches_with_odd_remainder(filter_slots)
+    after_filter_round = []
+    match = Match.new
+    tournament.matches << match
+    add_first_initialized_filtered_round_participant_to_match(match, filter_slots)
+    after_filter_round << match
+
+    tournament_memberships[filter_slots+1..-1].each_slice(2) do |pair|
+      match = Match.new
+      tournament.matches << match
+      add_participant_pair_to_match(match, pair)
+      after_filter_round << match
+    end
+    return after_filter_round
+  end
+
+  def build_filtered_round_matches_with_even_remainder(filter_slots)
+    tournament_memberships[filter_slots..-1].each_slice(2).with_object([]) do |pair, after_filter_round|
+      match = Match.new
+      tournament.matches << match
+      add_participant_pair_to_match(match, pair)
+      after_filter_round << match
+    end
+  end
+
+  def construct_round_arrays
+    # Each created nested array represents a round. The rightmost array represents the location for the winner
+    # The nil values in these subsequent arrays (rounds) represent future match locations
+    @output = [[nil]]
+    rounds.times do |i|
+      @output.unshift Array.new((2**(i+1))/2)
+    end
+  end
 end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -32,15 +32,11 @@ class Tournament < ActiveRecord::Base
   multisearchable against: [:title, :description],
                   using: { tsearch: { prefix: true }}
 
-  validates :title, presence: true, length: { maximum: 32 }
+  validates :title, presence: true, length: { maximum: 38 }
   validates :game, presence: true
   validates_with TimeValidator, on: :create
   validates_with TimeValidator, on: :update, if: :time_parameters?
   before_save :set_starts_at, if: :time_parameters?
-
-  # def open_bracket
-  #   Bracket.new(bracket, self)
-  # end
 
   def set_starts_at
     self.starts_at = Time.zone.parse("#{start_date} #{start_hour}:#{start_minute}#{start_period}")
@@ -51,113 +47,29 @@ class Tournament < ActiveRecord::Base
   end
 
   def start
-    reset_bracket
+    reset_bracket_components
     return self if tournament_memberships.empty?
-    rounds = calculate_rounds
-    self.bracket = [[nil]]
-    rounds.times do |i|
-      self.bracket.unshift(Array.new((2**(i+1))/2))
-    end
-    initialize_bracket
+    self.bracket = Bracket.new(self).construct
     self.started = true
     self.save
-    return self
   end
 
   def participant_count
-    if self.mode == 'individual'
+    case self.mode
+    when 'individual'
       self.users.count
-    elsif self.mode == 'team'
+    when 'team'
       self.teams.count
     end
   end
 
-  def reset_bracket
+  def reset_bracket_components
     self.user_showings.each(&:destroy)
+    self.team_showings.each(&:destroy)
     self.matches.destroy_all
     self.bracket = nil
   end
 
-  def calculate_rounds
-    Math.log2(tournament_memberships.count).ceil
-  end
-
-  def calculate_filtered_round_size(rounds)
-    2**(rounds - 1)
-  end
-
-  def calculate_number_of_filter_pairs(filtered_round_size)
-    tournament_memberships.count - filtered_round_size
-  end
-
-  def add_participant_pair_to_match(match, pair)
-    if self.mode == 'individual'
-      match.user_showings.push UserShowing.new(user_id: pair[0].user_id, top: true), UserShowing.new(user_id: pair[1].user_id)
-    elsif self.mode == 'team'
-      match.team_showings.push TeamShowing.new(team_id: pair[0].team_id, top: true), TeamShowing.new(team_id: pair[1].team_id)
-    end
-  end
-
-  def add_first_initialized_filtered_round_participant_to_match(match, filter_slots)
-    if self.mode == 'individual'
-      match.user_showings.push UserShowing.new(user_id: tournament_memberships[filter_slots..-1].first.user_id)
-    elsif self.mode == 'team'
-      match.team_showings.push TeamShowing.new(team_id: tournament_memberships[filter_slots..-1].first.team_id)
-    end
-  end
-
-  def add_filled_matches_to_filter_round(filter_slots)
-    tournament_memberships.first(filter_slots).each_slice(2).with_object([]) do |pair,obj|
-      match = Match.new
-      self.matches << match
-      add_participant_pair_to_match(match, pair)
-      obj << match
-    end
-  end
-
-  def build_filtered_round_matches_with_odd_remainder(filter_slots)
-    after_filter_round = []
-    match = Match.new
-    self.matches << match
-    add_first_initialized_filtered_round_participant_to_match(match, filter_slots)
-    after_filter_round << match
-
-    tournament_memberships[filter_slots+1..-1].each_slice(2) do |pair|
-      match = Match.new
-      self.matches << match
-      add_participant_pair_to_match(match, pair)
-      after_filter_round << match
-    end
-    return after_filter_round
-  end
-
-  def build_filtered_round_matches_with_even_remainder(filter_slots)
-    tournament_memberships[filter_slots..-1].each_slice(2).with_object([]) do |pair, after_filter_round|
-      match = Match.new
-      self.matches << match
-      add_participant_pair_to_match(match, pair)
-      after_filter_round << match
-    end
-  end
-
-  def initialize_bracket
-    ## Takes the number of rounds and finds how many slots are contested
-    ## for the round following the "filter" round. It then takes these contested
-    ## slots, multiplies them by two and shoves that amount of players into
-    ## the "filter" round. The remainder get moved onto the end of the next round.
-    rounds = calculate_rounds
-    filtered_round_size = calculate_filtered_round_size(rounds)
-    filter_pairs = calculate_number_of_filter_pairs(filtered_round_size)
-    filter_slots = filter_pairs*2
-
-    self.bracket[0][0..((filter_slots/2)-1)] = add_filled_matches_to_filter_round(filter_slots)
-
-    if tournament_memberships[filter_slots..-1].length.odd?
-      self.bracket[1][(filter_pairs/2)..-1] = build_filtered_round_matches_with_odd_remainder(filter_slots)
-    else
-      self.bracket[1][(filter_pairs/2)..-1] = build_filtered_round_matches_with_even_remainder(filter_slots)
-    end
-  end
 
   def find_bottom_participant(match)
     if match.tournament.mode == "individual"

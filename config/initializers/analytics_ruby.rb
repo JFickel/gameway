e5bdcaa2146a8480cc631d766f12557648ca7b5c
James Fickel <jamesfickel@gmail.com>
2013-11-14 21:08:09 -0800

    testing why heroku deploy is throwing error for segmentio initializer

--- config/initializers/analytics_ruby.rb
+++ config/initializers/analytics_ruby.rb
@@ -1,6 +1,17 @@
 Analytics = AnalyticsRuby
 
 Analytics.init({
+  puts "HELLO HELLO"
+  puts "HELLO HELLO"
+  puts "HELLO HELLO"
+  puts "HELLO HELLO"
+  puts ENV['SEGMENTIO_SECRET']
+  puts ENV.keys
+  puts "HELLO"
+  puts "HELLO"
+  puts "HELLO"
+  puts "HELLO"
+  puts "HELLO"
   secret: ENV['SEGMENTIO_SECRET'],
   on_error: Proc.new { |status, msg| print msg }  # Optional error handler
 })

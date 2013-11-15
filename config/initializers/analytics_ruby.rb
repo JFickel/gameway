9938016b2602f68f5cfbc296d9d8836d331017ff
James Fickel <jamesfickel@gmail.com>
2013-11-14 21:09:46 -0800

    syntax error in analytics heroku test

--- config/initializers/analytics_ruby.rb
+++ config/initializers/analytics_ruby.rb
@@ -1,17 +1,18 @@
 Analytics = AnalyticsRuby
 
+puts "HELLO HELLO"
+puts "HELLO HELLO"
+puts "HELLO HELLO"
+puts "HELLO HELLO"
+puts ENV['SEGMENTIO_SECRET']
+puts ENV.keys
+puts "HELLO"
+puts "HELLO"
+puts "HELLO"
+puts "HELLO"
+puts "HELLO"
+
 Analytics.init({
-  puts "HELLO HELLO"
-  puts "HELLO HELLO"
-  puts "HELLO HELLO"
-  puts "HELLO HELLO"
-  puts ENV['SEGMENTIO_SECRET']
-  puts ENV.keys
-  puts "HELLO"
-  puts "HELLO"
-  puts "HELLO"
-  puts "HELLO"
-  puts "HELLO"
   secret: ENV['SEGMENTIO_SECRET'],
   on_error: Proc.new { |status, msg| print msg }  # Optional error handler
 })

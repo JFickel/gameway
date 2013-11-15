76b333e6bf60b1ce14c18fa8ccb2dc86007588dd
James Fickel <jamesfickel@gmail.com>
2013-11-14 21:48:57 -0800

    apparently config vars are not available during slug compilation

--- config/initializers/analytics_ruby.rb
+++ config/initializers/analytics_ruby.rb
@@ -1,18 +1,6 @@
 Analytics = AnalyticsRuby
 
-puts "HELLO HELLO"
-puts "HELLO HELLO"
-puts "HELLO HELLO"
-puts "HELLO HELLO"
-puts ENV['SEGMENTIO_SECRET']
-puts ENV.keys
-puts "HELLO"
-puts "HELLO"
-puts "HELLO"
-puts "HELLO"
-puts "HELLO"
-
 Analytics.init({
-  secret: ENV['SEGMENTIO_SECRET'],
+  secret: "u0yahi9lt5q4z9muohu7",
   on_error: Proc.new { |status, msg| print msg }  # Optional error handler
 })

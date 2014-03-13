b061bc76008eaf0172c003dfa4cdd6846624b679
James Fickel <jamesfickel@gmail.com>
2014-03-12 18:51:18 -0700

    logging ember transitions and added some routes

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,6 +1,10 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 Gameway.Router.map(function() {
-  
-  // this.resource('posts');
+  this.resource('tournaments', function() {
+    this.route('new');
+  });
+  this.resource('tournament', { path: '/tournaments/:tournament_id' }, function() {
+    this.route('edit');
+  });
 });

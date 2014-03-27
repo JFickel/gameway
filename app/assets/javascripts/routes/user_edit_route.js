a60409ec39c73cd5ef3f1abe249caf9fe3af9d8b
James Fickel <jamesfickel@gmail.com>
2014-03-27 12:52:09 -0700

    removed debugger

--- app/assets/javascripts/routes/user_edit_route.js
+++ app/assets/javascripts/routes/user_edit_route.js
@@ -1,6 +1,5 @@
 Gameway.UserEditRoute = Gameway.Route.extend({
   beforeModel: function(transition) {
-    debugger;
     if (transition.params.user.user_id !== this.get('currentUser.id')) {
       this.transitionTo('index')
     }

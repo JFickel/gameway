12ac57fda89f746f1740635a4bd77fd0754cf213
James Fickel <jamesfickel@gmail.com>
2014-04-02 20:02:56 -0700

    added image uploads with carrierwave -- using the html5 file api to handle image uploads with ember

---
+++ app/assets/javascripts/controllers/modals_avatar_upload_controller.js
@@ -0,0 +1,43 @@
+Gameway.ModalsAvatarUploadController = Gameway.Controller.extend({
+  actions: {
+    cancelAvatarUpload: function() {
+      this.send('closeModal')
+    },
+    uploadAvatar: function() {
+      var file = $('#avatarUploadField')[0].files[0],
+          reader = new FileReader(),
+          thisController = this;
+
+      thisController.set('currentlyUploading', true);
+      thisController.set('fileExtension', file.name.match(/\..*/)[0]);
+
+      // When finished reading the file, send AJAX request to save
+      reader.onload = (function(theFile) {
+        return function(e) {
+          var fileData = e.target.result;
+          $.ajax({
+            type: 'PATCH',
+            url: 'users/' + thisController.get('currentUser.id'),
+            data: { authenticity_token: Gameway.gon.get('authenticityToken'),
+                    file_data: fileData,
+                    file_extension: thisController.get('fileExtension')
+            },
+            success: function(data) {
+              if (data.errors) {
+                data.errors.forEach(function(error) {
+                  Gameway.flashController.pushObject({message: error,
+                                                      type: 'alert-danger'})
+                })
+              } else {
+                thisController.send('closeModal')
+                thisController.set('currentUser.avatarUrl', data.avatar_url + '?' + Math.random())
+              }
+            }
+          })
+        }
+      })(file);
+
+      reader.readAsDataURL(file)
+    }
+  }
+})
--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -2,5 +2,6 @@ attr = DS.attr;
 
 Gameway.User = DS.Model.extend({
   email: attr('string'),
-  lolAccountId: DS.belongsTo('lolAccount')
+  lolAccountId: DS.belongsTo('lolAccount'),
+  avatarUrl: attr('string')
 })

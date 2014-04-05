03f8e8a98e48c80581bf15b5b05fb29402eaa91a
James Fickel <jamesfickel@gmail.com>
2014-04-05 03:15:03 -0700

    added some basic logic for transitioning users after sign in -- also hiding elements depending on whether the user has the associations

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitchtv
     @user, flash[:first_twitch_auth] = User.find_for_twitchtv_oauth(request.env["omniauth.auth"])
+    flash[:twitch_auth] = true unless flash[:first_twitch_auth]
     sign_in @user
     redirect_to root_path
   end

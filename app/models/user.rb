c1cfec27f77fb1c55068c2481fd3c560c3cb98bc
James Fickel <jamesfickel@gmail.com>
2014-04-05 18:31:58 -0700

    small fix to find_for_twitchtv_oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,11 @@ class User < ActiveRecord::Base
     return [user, false] if user.present?
 
     if user = User.where(email: auth.info.email).first
-      user.update_attributes(provider: auth.provider, uid: auth.uid, confirmation_token: nil, confirmed_at: Time.current)
+      user.update_attributes(name: auth.info.nickname,
+                             provider: auth.provider,
+                             uid: auth.uid,
+                             confirmation_token: nil,
+                             confirmed_at: Time.current)
       return user, false
     end
 
@@ -30,7 +34,6 @@ class User < ActiveRecord::Base
                     email: auth.info.email,
                     password: Devise.friendly_token[0,20])
 
-    # user.name = auth.info.name   # assuming the user model has a name
     # user.image = auth.info.image # assuming the user model has an image
 
     user.skip_confirmation!

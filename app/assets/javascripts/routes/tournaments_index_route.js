d558a3e8e4c19f35aaa52b5680b01a14733e3c0b
James Fickel <jamesfickel@gmail.com>
2014-04-08 02:59:33 -0700

    cleared out commented code

--- app/assets/javascripts/routes/tournaments_index_route.js
+++ app/assets/javascripts/routes/tournaments_index_route.js
@@ -2,8 +2,5 @@ Gameway.TournamentsIndexRoute = Gameway.Route.extend({
   model: function() {
     var region = this.get('currentUser.lolAccount.region') || "na"
     return this.store.find('tournament', { lol_region: region })
-  },
-  // action: {
-
-  // }
+  }
 });
